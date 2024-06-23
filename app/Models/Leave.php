5251081f0878b610744ef4670db3c3f06cc66e38
dandelionMNS <dandelionmns197@gmail.com>
2024-06-24 05:28:36 +0800

    Modify: Alter leaves table, include leav_types, provide default data

--- app/Models/Leave.php
+++ app/Models/Leave.php
@@ -10,6 +10,7 @@ class Leave extends Model
     use HasFactory;
     protected $fillable = [
         "user_id",
+        'leave_type_id',
         "documents",
         "start",
         "end",
@@ -18,5 +19,8 @@ class Leave extends Model
 
     public function user(){
         return $this->belongsTo(User::class);
+    }
+    public function leave_type(){
+        return $this->belongsTo(Leave_type::class);
     }
 }
